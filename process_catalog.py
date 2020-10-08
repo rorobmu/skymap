6ea31d1a5ec6737dad1823033fd87b8a60fca438
rducroiset <rducroiset@aliapur.fr>
2020-10-08 17:31:34 +0200

    add tools and clean code

--- process_catalog.py
+++ process_catalog.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 def process_star_catalog(constellations):
+   
     colwidths = [4,10,11,6,6,4,1,1,1,5,2,9,2,2,4,1,2,2,2,2,2,4,1,2,2,2,6,6,5,
                  1,1,5,1,5,1,5,1,20,1,6,6,1,5,4,4,2,3,1,4,6,4,2,1]
     colnames = ['HR_number', 'Name', 'DM', 'HD', 'SAO', 'FK5','IRflag','r_IRflag', 'Multiple', 'ADS'
@@ -13,20 +14,28 @@ def process_star_catalog(constellations):
                 ,'RAh','RAm', 'RAs', 'DE-', 'DEd', 'DEm', 'DEs', 'GLON', 'GLAT', 'Vmag', 'n_Vmag', 'u_Vmag', 'B-V', 'u_B-V'
                 , 'U-B', 'u_U-B', 'R-I', 'n_R-I', 'SpType', 'n_SpType', 'pmRA', 'pmDE', 'n_Parallax', 'Parallax', 'RadVel'
                 , 'n_RadVel', 'l_RotVel', 'RotVel', 'u_RotVel', 'Dmag', 'Sep', 'MultID', 'MultCnt', 'NoteFlag']
-    
+    """
     df = pd.read_fwf('catalog.dat', widths=colwidths, names=colnames)
     df_name_notna = df['Name'].notna()
     df_stars = df[df['Name'].str.contains('UMa').fillna(False)]
-    df_stars = df_stars[df_stars['Vmag']<=6.5]
+    df_stars = df_stars[df_stars['Vmag']<=8]
     df_stars['RA'] = df_stars['RAh'] + df_stars['RAm']/60 + df_stars['RAs']/3600
     df_stars['Dec'] = df_stars['DEd'] + df_stars['DEm']/60 + df_stars['DEs']/3600
     df_stars['Dec'] = df_stars['DE-'] + df_stars['Dec'].astype(str)
     df_stars['Dec'] = df_stars['Dec'].astype(float)
-    
+    """
     df_hyg = pd.read_csv('hygdata_v3.csv', sep=',')
-    df_stars = df_hyg[df_hyg['bf'].str.contains(constellations, case=False, regex=True).fillna(False)]
-    df_stars = df_stars[df_stars['mag']<=6.5]
+    #df_stars = df_hyg[df_hyg['bf'].str.contains(constellations, case=False, regex=True).fillna(False)]
+    
+    df_stars = df_hyg[df_hyg['mag']<=7]
+    df_stars = df_stars[df_stars['dec']>=-55]
+    #remove sun
+    df_stars = df_stars[df_stars['proper']!='Sol']
     df_stars.to_csv('stars.csv', sep=';', index=False)
 
 
+def get_df_visible_stars(df_full):
+    df_visible_stars = df_full[df_full['mag']<=4]
+    df_visible_stars = df_visible_stars[df_visible_stars['dec']>=-40]
+    return df_visible_stars
 
